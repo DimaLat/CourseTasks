d8c59f4d4f788119c9190d523598971a1e232a61
DimaLat <dima.latyshionok@gmail.com>
2020-02-12 23:34:03 +0300

    add 3 classes: XmlSaver,JsonSaver,FormatChooser. Refactor some object names

--- Task1/script_code.py
+++ Task1/script_code.py
@@ -12,17 +12,17 @@ class Rooms:
     """This class is for list of parsing rooms"""
 
     def __init__(self, file_location):
-        self.list_object = parsing_reading(file_location)
+        self.list_object = parse_data(file_location)
 
 
 class Students:
     """This class is for list of parsing students"""
 
     def __init__(self, file_location):
-        self.list_object = parsing_reading(file_location)
+        self.list_object = parse_data(file_location)
 
 
-class Unite:
+class Uniter:
     """This class is for unite data from objects Rooms and Students """
 
     def __init__(self, room: Rooms, student: Students):
@@ -39,39 +39,54 @@ class Unite:
         return relocation_list
 
 
-def parsing_reading(parsing_filename):
+class JsonSaver:
+    def __init__(self, students_relocation_list):
+        self.save_as_json(students_relocation_list)
+
+    @staticmethod
+    def save_as_json(relocation_list):
+        """This function is for saving parsing data as json file"""
+        with open(os.path.join('files/students_relocation.json'), 'w') as file:
+            json.dump(relocation_list, file, indent=4)
+
+
+class XmlSaver:
+    def __init__(self, students_relocation_list):
+        self.save_as_xml(students_relocation_list)
+
+    @staticmethod
+    def save_as_xml(relocation_list):
+        """This function is for saving parsing data as xml file"""
+        xml = dicttoxml.dicttoxml(relocation_list)
+        dom = parseString(xml)
+        with open(os.path.join('files/students_relocation.xml'), 'w') as file:
+            file.write(str(dom.toprettyxml()))
+
+
+class Format_Chooser:
+    def __init__(self, format, students_relocation_list):
+        self.format_choose(format, students_relocation_list)
+
+    @staticmethod
+    def format_choose(format, students_relocation_list):
+        """This function is for choosing in which format data will be parsed"""
+        if format == "json":
+            JsonSaver(students_relocation_list)
+        elif format == "xml":
+            XmlSaver(students_relocation_list)
+
+
+def parse_data(parsing_filename):
     """This function is for parsing data from files"""
     with open(os.path.join(parsing_filename), 'r') as file:
         return json.load(file)
 
 
-def save_as_json(relocation_list):
-    """This function is for saving parsing data as json file"""
-    with open(os.path.join('files/students_relocation.json'), 'w') as file:
-        json.dump(relocation_list, file, indent=4)
-
-
-def save_as_xml(relocation_list):
-    """This function is for saving parsing data as xml file"""
-    xml = dicttoxml.dicttoxml(relocation_list)
-    dom = parseString(xml)
-    with open(os.path.join('files/students_relocation.xml'), 'w') as file:
-        file.write(str(dom.toprettyxml()))
-
-
-def format_choose(format, students_relocation_list):
-    """This function is for choosing in which format data will be parsed"""
-    if format == "json":
-        save_as_json(students_relocation_list)
-    elif format == "xml":
-        save_as_xml(students_relocation_list)
-
-
 def main():
     rooms = Rooms(file_location=rooms_location)
     students = Students(file_location=students_location)
-    students_relocation_list = Unite(student=students, room=rooms).relocate()
-    format_choose(format, students_relocation_list)
+    students_relocation_list = Uniter(student=students, room=rooms).relocate()
+    Format_Chooser(format, students_relocation_list)
 
 
 if __name__ == '__main__':
