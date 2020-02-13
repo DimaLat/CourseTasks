67619aa60c387b205c2c67b930ffbdb2d1a88a5a
DimaLat <dima.latyshionok@gmail.com>
2020-02-14 00:55:45 +0300

    start overriding magic methods. override method __eq__

--- Task2/script_code.py
+++ Task2/script_code.py
@@ -1,7 +1,18 @@
+import numpy
+
+
 class Version:
-    def __init__(self, version):
-        pass
-#
+    def __init__(self, version: str):
+        self.version = version
+
+    def __eq__(self, other):
+        self.version = list(self.version)
+        other.version = list(other.version)
+        if numpy.all(self.version == other.version):
+            return True
+        elif numpy.all(self.version != other.version):
+            return False
+
 
 def main():
     to_test = [
@@ -17,7 +28,6 @@ def main():
         assert Version(version_1) < Version(version_2), 'le failed'
         assert Version(version_2) > Version(version_1), 'ge failed'
         assert Version(version_2) != Version(version_1), 'neq failed'
-
 
 if __name__ == "__main__":
     main()
