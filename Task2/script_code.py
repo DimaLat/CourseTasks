2472330ea3a680c65b0864085243b164a7d2475f
DimaLat <dima.latyshionok@gmail.com>
2020-02-16 23:41:08 +0300

    change methods names, replace lt to gt method,all asserts are passed

--- Task2/script_code.py
+++ Task2/script_code.py
@@ -5,6 +5,8 @@ from functools import total_ordering
 class Version:
     def __init__(self, version: str):
         self.version = version
+        self.first_part = self.get_first_part()
+        self.second_part = self.get_second_part()
 
     def convert_to_one_format(self):
         """method for converting versions to one format"""
@@ -13,34 +15,38 @@ class Version:
         version = version.replace('-', '|')
         version = version.replace('alpha', 'a')
         version = version.replace('beta', 'b')
-        version = version.replace('1b', '1-b')
-        standard_version = version
-        print(standard_version)
-        return standard_version
+        version = version.replace('1b', '1|b')
+        standard_format_version = version
+        return standard_format_version
 
     def __eq__(self, other):
         return self.convert_to_one_format() == other.convert_to_one_format()
 
-    def __lt__(self, other):
-        return self.convert_to_one_format() < other.convert_to_one_format()
+    def __gt__(self, other):
+        if self.first_part != other.first_part:
+            return self.first_part > other.first_part
+        elif self.second_part == '':
+            return True
+        elif other.second_part == '':
+            return False
 
-    def get_letter_part(self):
-        """method for taking letter part from version"""
-
-        if '|' in self.convert_to_one_format():
-            letter_part = self.convert_to_one_format().split('|')[1]
-        else:
-            letter_part = ''
-        return letter_part
-
-    def get_numeric_part(self):
-        """method for taking numeric part from version"""
+    def get_first_part(self):
+        """method for taking the first part from version which contains numbers"""
 
         if '|' in self.convert_to_one_format():
-            numeric_part = self.convert_to_one_format().split('|')[0]
+            first_part = self.convert_to_one_format().split('|')[0]
         else:
-            numeric_part = version
-        return numeric_part
+            first_part = self.version
+        return first_part
+
+    def get_second_part(self):
+        """method for taking the second part from version which contains numbers and letters"""
+
+        if '|' in self.convert_to_one_format():
+            second_part = self.convert_to_one_format().split('|')[1]
+        else:
+            second_part = ''
+        return second_part
 
 
 def main():
